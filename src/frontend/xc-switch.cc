7197d536063259559812e50149481361a28cb369
Sadjad Fouladi <sfouladi@gmail.com>
2017-04-28 14:48:23 -0700

    Read all the frames and outputs and put them into arrays.

--- src/frontend/xc-switch.cc
+++ src/frontend/xc-switch.cc
@@ -8,18 +8,21 @@
 #include <iostream>
 #include <string>
 #include <chrono>
+#include <vector>
 
 #include "ivf.hh"
 #include "yuv4mpeg.hh"
 #include "frame.hh"
 #include "decoder.hh"
 #include "encoder.hh"
+#include "uncompressed_chunk.hh"
+#include "vp8_raster.hh"
 
 using namespace std;
 
 void usage_error( const string & program_name )
 {
-  cerr << program_name << " INPUT1 INPUT2" << endl;
+  cerr << program_name << " FROM TO" << endl;
 }
 
 int main( int argc, char *argv[] )
@@ -33,6 +36,44 @@ int main( int argc, char *argv[] )
       usage_error( argv[ 0 ] );
       return EXIT_FAILURE;
     }
+
+    IVF ivf[] = { { argv[ 1 ] }, { argv[ 2 ] } };
+
+    uint16_t width = ivf[ 0 ].width();
+    uint16_t height = ivf[ 0 ].height();
+
+    vector<RasterHandle> outputs[ 2 ];
+    vector<pair<Optional<KeyFrame>, Optional<InterFrame> > > frames[ 2 ];
+
+    Decoder decoder[] = { { width, height }, { width, height } };
+
+    assert( ivf[0].frame_count() == ivf[1].frame_count() );
+
+    for ( size_t frame_number = 0; frame_number < ivf[0].frame_count(); frame_number++ ) {
+      UncompressedChunk uncompressed_chunk[] =
+       {
+         { ivf[ 0 ].frame( frame_number ), width, height, false },
+         { ivf[ 1 ].frame( frame_number ), width, height, false }
+       };
+
+       for ( size_t i = 0; i < 2; i++ ) {
+         if ( uncompressed_chunk[ i ].key_frame() ) {
+           KeyFrame frame = decoder[ i ].parse_frame<KeyFrame>( uncompressed_chunk[ i ] );
+           pair<bool, RasterHandle> output = decoder[ i ].decode_frame( frame );
+
+           frames[ i ].emplace_back( move( frame ), Optional<InterFrame>() );
+           outputs[ i ].push_back( output.second );
+         }
+         else {
+           InterFrame frame = decoder[ i ].parse_frame<InterFrame>( uncompressed_chunk[ i ] );
+           pair<bool, RasterHandle> output = decoder[ i ].decode_frame( frame );
+
+           frames[ i ].emplace_back( Optional<KeyFrame>(), move( frame ) );
+           outputs[ i ].push_back( output.second );
+         }
+       }
+    }
+
 
   }
   catch ( const exception &  e ) {
